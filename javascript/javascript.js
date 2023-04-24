c03324f6fcd7d91c32390decbcbd12e2062e7584
ishimweaxcel <aishimwe811@gmail.com>
2023-04-24 20:25:39 +0200

    added a javaScript file

---
+++ javascript/javascript.js
@@ -0,0 +1,19 @@
+//string concantinng
+//variable declaration
+//let, const //data type
+
+let firstName = "Ishimwe";
+let secondName = "Axcel";
+let thirdName = "Brilliant"
+
+console.log("Welcome to my home page");
+console.log(firstName + " " + secondName);
+console.log(2+2);
+console.log("4");
+
+if(firstName === "Ishimwe" && secondName === "Axcel"){
+    console.log("correct!!!")
+}
+else if(thirdName === "Brilliant"){
+    console.log("nice try my friend !!!")
+}
